9ee7277bae8c6d3876067fb334999864a2df86da
drewros <arosenberg3@hawk.iit.edu>
2022-11-08 20:34:09 -0600

    Added JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
-
-
-
-
-
-
-
-
+  function alertText(){
+    var enter = documnet.getElementById('entryinput');
+    var out = documnet.getElementById('textoutput');
+    out.innerHTML = enter.value;
+    alert('Andrew Rosenberg: ' + enter.value);
+  }
+    var enterBtn = document.getElementById('entrybutton');
+    enterBtn.addEventListener('click', alertText);
+}
 window.addEventListener('load', init);
