e376a50a9c763d7f541c050a93a726386e18aa0e
drewros <arosenberg3@hawk.iit.edu>
2022-11-08 20:47:28 -0600

    Updated JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 function init(){
-//add your javascrip between these two lines of code
   function alertText(){
-    var enter = documnet.getElementById('entryinput').value;
-    var out = documnet.getElementById('textoutput');
-    out.innerHTML = enter.value;
-    alert('Andrew Rosenberg: ' + enter.value);
+    var inpt = document.getElementById('entryinput');
+    var otpt = document.getElementById('textoutput');
+    otpt.innerHTML = inpt.value;
+    alert('Andrew Rosnberg: ' + inpt.value);
   }
-    var enterBtn = document.getElementById('entrybutton');
-    enterBtn.addEventListener('click', alertText);
+  var clckBtn = document.getElementById('entrybutton');
+  clckBtn.addEventListener('click', alertText);
 }
 window.addEventListener('load', init);
